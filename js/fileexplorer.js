60bbb34fe851822d46084df1322e4a4e76d6d17f
Andriy Skochypets <skoff.ua@gmail.com>
2015-07-14 23:46:38 +0300

    Added keyboard functionality and scroll bar.

--- js/fileexplorer.js
+++ js/fileexplorer.js
@@ -21,5 +21,48 @@ app.controller("fileExplorerCtrl", function ($scope, $http) {
 			$scope.getFileList(newUrl.join("\\"));	
 		}		
 	}
+	// List controll with keyboard
+	$scope.keyControl = function (event, itemName) {
+		var key = event.keyCode;
+
+		if (key === 8) {
+			event.preventDefault();
+			$scope.stepBack($scope.dataObj.root);	
+		} else if (key === 13) {
+			event.preventDefault();
+			$scope.getFileList($scope.dataObj.root + itemName + '\\', event);
+		} else if (key === 37) {
+			event.preventDefault();
+			$scope.focusElem(event, "prev");
+		} else if (key === 38) {
+			event.preventDefault();
+			$scope.focusElem(event, "prev");
+		} else if (key === 39) {
+			event.preventDefault();
+			$scope.focusElem(event, "next");
+		} else if (key === 40) {
+			event.preventDefault();
+			$scope.focusElem(event, "next");
+		}
+	}
+	// Focus element with keyboard
+	$scope.focusElem = function (event, direction) {
+		var elem = event.target;
+		
+		if (direction === "next") {
+			if (elem.nextElementSibling) {
+				elem.nextElementSibling.focus();
+			} else {
+				elem.parentElement.firstElementChild.focus();
+			}
+		} else if (direction === "prev") {
+			if (elem.previousElementSibling) {
+				elem.previousElementSibling.focus();
+			} else {
+				elem.parentElement.lastElementChild.focus();
+			}
+		}
+	} 
+
 	$scope.getFileList();
 });
