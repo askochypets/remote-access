58059843ab0f2cf1f5a3cb300c9050fffcff313d
Andiy Skochypets <skoff.ua@gmail.com>
2015-09-15 23:58:00 +0300

    Added tcp protocol into client.js

--- js/fileexplorer.js
+++ js/fileexplorer.js
@@ -1,24 +1,37 @@
+var	net = require('net'),
+	client = new net.Socket();
+
 var app = angular.module("fileExplorer", []);
 
-app.controller("fileExplorerCtrl", function ($scope, $http) {
-	$scope.getFileList = function (dirPath) {
-		$http.post("127.0.0.1/filelist", {dirPath: dirPath})
-		.success(function (data) {
-			$scope.dataObj = {
-				files: data.files,
-				root: data.root,
-			}
-		})
-		.error(function () {
-			console.log("Ajax sent fail!");
+app.controller("fileExplorerCtrl", function ($scope) {
+	$scope.getConnect = function () {
+		client.connect(1337, '127.0.0.1', function() {
+		    console.log('CONNECTED TO: 127.0.0.1 1337');
 		});
+
+		client.on('data', function(response) {
+			var data = JSON.parse(response),
+				newData = JSON.parse(data);
+
+		    $scope.dataObj = {
+				files: newData.files,
+				root: newData.root,
+			}
+		});
+
+		client.on('close', function() {
+		    console.log('Connection closed');
+		});
+	}
+	$scope.getNewData = function (path) {
+		client.write(path);
 	}
 	$scope.stepBack = function (postUrl) {
 		if (postUrl !== "" && postUrl !== undefined) {
 			var newUrl = postUrl.split("\\");
 			
 			newUrl.splice(-2, 1);
-			$scope.getFileList(newUrl.join("\\"));	
+			//$scope.getFileList(newUrl.join("\\"));	
 		}		
 	}
 	// List controll with keyboard
@@ -30,7 +43,7 @@ app.controller("fileExplorerCtrl", function ($scope, $http) {
 			$scope.stepBack($scope.dataObj.root);	
 		} else if (key === 13) {
 			event.preventDefault();
-			$scope.getFileList($scope.dataObj.root + itemName + '\\', event);
+			//$scope.getFileList($scope.dataObj.root + itemName + '\\', event);
 		} else if (key === 37) {
 			event.preventDefault();
 			$scope.focusElem(event, "prev");
